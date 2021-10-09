64840e954328d3a7ff3dface4a94ae98bf6637b7
Moros1138 <moros1138@gmail.com>
2021-10-09 09:08:31 -0400

    descriptor tables

---
+++ include/descriptor_tables.h
@@ -0,0 +1,99 @@
+// 
+// descriptor_tables.h - Defines the interface for initialising the GDT and IDT.
+//                       Also defines needed structures.
+//                       Based on code from Bran's kernel development tutorials.
+//                       Rewritten for JamesM's kernel development tutorials.
+//
+#ifndef _MOROS_DESCRIPTOR_TABLES_H
+#define _MOROS_DESCRIPTOR_TABLES_H
+
+#include "types.h"
+#include "memory.h"
+
+// Initialisation function is publicly accessible.
+void init_descriptor_tables();
+
+
+// This structure contains the value of one GDT entry.
+// We use the attribute 'packed' to tell GCC not to change
+// any of the alignment in the structure.
+struct gdt_entry_struct
+{
+    uint16_t limit_low;           // The lower 16 bits of the limit.
+    uint16_t base_low;            // The lower 16 bits of the base.
+    uint8_t  base_middle;         // The next 8 bits of the base.
+    uint8_t  access;              // Access flags, determine what ring this segment can be used in.
+    uint8_t  granularity;
+    uint8_t  base_high;           // The last 8 bits of the base.
+} __attribute__((packed));
+
+typedef struct gdt_entry_struct gdt_entry_t;
+
+// This struct describes a GDT pointer. It points to the start of
+// our array of GDT entries, and is in the format required by the
+// lgdt instruction.
+struct gdt_ptr_struct
+{
+    uint16_t limit;               // The upper 16 bits of all selector limits.
+    uint32_t base;                // The address of the first gdt_entry_t struct.
+} __attribute__((packed));
+
+typedef struct gdt_ptr_struct gdt_ptr_t;
+
+// A struct describing an interrupt gate.
+struct idt_entry_struct
+{
+    uint16_t base_lo;             // The lower 16 bits of the address to jump to when this interrupt fires.
+    uint16_t sel;                 // Kernel segment selector.
+    uint8_t  always0;             // This must always be zero.
+    uint8_t  flags;               // More flags. See documentation.
+    uint16_t base_hi;             // The upper 16 bits of the address to jump to.
+} __attribute__((packed));
+
+typedef struct idt_entry_struct idt_entry_t;
+
+// A struct describing a pointer to an array of interrupt handlers.
+// This is in a format suitable for giving to 'lidt'.
+struct idt_ptr_struct
+{
+    uint16_t limit;
+    uint32_t base;                // The address of the first element in our idt_entry_t array.
+} __attribute__((packed));
+
+typedef struct idt_ptr_struct idt_ptr_t;
+
+// These extern directives let us access the addresses of our ASM ISR handlers.
+extern "C" void isr0 ();
+extern "C" void isr1 ();
+extern "C" void isr2 ();
+extern "C" void isr3 ();
+extern "C" void isr4 ();
+extern "C" void isr5 ();
+extern "C" void isr6 ();
+extern "C" void isr7 ();
+extern "C" void isr8 ();
+extern "C" void isr9 ();
+extern "C" void isr10();
+extern "C" void isr11();
+extern "C" void isr12();
+extern "C" void isr13();
+extern "C" void isr14();
+extern "C" void isr15();
+extern "C" void isr16();
+extern "C" void isr17();
+extern "C" void isr18();
+extern "C" void isr19();
+extern "C" void isr20();
+extern "C" void isr21();
+extern "C" void isr22();
+extern "C" void isr23();
+extern "C" void isr24();
+extern "C" void isr25();
+extern "C" void isr26();
+extern "C" void isr27();
+extern "C" void isr28();
+extern "C" void isr29();
+extern "C" void isr30();
+extern "C" void isr31();
+
+#endif // _MOROS_DESCRIPTOR_TABLES_H
