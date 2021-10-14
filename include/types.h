519c34353c6e42259cbd774ce8e146be562acb92
Moros Smith <moros1138@gmail.com>
2021-10-13 20:10:22 -0400

    fix size_t

--- include/types.h
+++ include/types.h
@@ -1,14 +1,14 @@
 #ifndef _MOROS_TYPES_H
 #define _MOROS_TYPES_H
 
-typedef char            int8_t;
-typedef unsigned char   uint8_t;
-typedef short           int16_t;
-typedef unsigned short  uint16_t;
-typedef int             int32_t;
-typedef unsigned int    uint32_t;
+typedef char                int8_t;
+typedef unsigned char       uint8_t;
+typedef short               int16_t;
+typedef unsigned short      uint16_t;
+typedef int                 int32_t;
+typedef unsigned int        uint32_t;
 
-typedef unsigned int    size_t;
+typedef long unsigned int   size_t;
 
 
 
