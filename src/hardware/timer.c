7c386391d1668e096b12042bf30bdb31ddd8156e
Moros1138 <moros1138@gmail.com>
2021-12-12 00:34:12 -0500

    move comment

--- src/hardware/timer.c
+++ src/hardware/timer.c
@@ -15,6 +15,8 @@ void timer_init()
     register_interrupt_handler(IRQ0, &timer_callback);
     
     outb(0x43, 0x36);
+    // divisor = 1193180 / frequency;
+    // because we want a divisor of about 1000, frequency = 1193
     uint32_t divisor = 1193180 / 1193;
 
     // divisor low byte
@@ -22,8 +24,6 @@ void timer_init()
     // divisor high byte
     outb(0x40, (divisor & 0xff00) >> 8);
 
-    // divisor = 1193180 / frequency;
-    // because we want a divisor of about 1000, frequency = 1193
 }
 
 uint32_t timer_get_time_point()
