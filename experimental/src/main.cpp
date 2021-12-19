eba7a317bbae370a1771eba027bdb8a437a8d27c
Moros1138 <moros1138@gmail.com>
2021-12-19 17:33:20 -0500

    update test

--- experimental/src/main.cpp
+++ experimental/src/main.cpp
@@ -6,38 +6,13 @@
 #include "moros_allocator.h"
 #include "moros_vector.h"
 
-// #include "string"
-
 int main()
 {
-    // std::vector<int, MorOS::allocator<int>> test;
-    MorOS::vector<int> test = {1, 2, 3, 4, 5};
-    // test.reserve(128);
-    // for(int i = 0; i < 128; i++)
-    // {
-    //     test.push_back(i);
-    // }
-    
-    std::cout << test.size() << " " << test.capacity() << std::endl;
-    
-    // test.resize(64);
-    // std::cout << test.size() << " " << test.capacity() << std::endl;
-    
-    // for(int i = 0; i < 128; i++)
-    // {
-    //     test.push_back(i);
-    // }
+    MorOS::vector<int> test{1, 2, 3, 4, 5};
+    std::vector<int, MorOS::allocator<int>> test2{1, 2, 3, 4, 5};
 
-    // for(auto &i : test)
-    //     std::cout << i << " ";
-    
-    // std::cout << std::endl;
-        
-    // std::cout << test.size() << " " << test.capacity() << std::endl;
-    
-    // test.shrink_to_fit();
-
-    // std::cout << test.size() << " " << test.capacity() << std::endl;
+    std::cout << test[5] << std::endl;
+    std::cout << test.at(5) << std::endl;
 
     return 0;
 }
