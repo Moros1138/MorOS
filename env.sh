2b7e06f94b1e208224da585c0a0a0bc58951e996
Moros1138 <moros1138@gmail.com>
2021-12-21 22:43:09 -0500

    make build commands short

--- env.sh
+++ env.sh
@@ -6,3 +6,20 @@ switch()
     cp $MOROS_ROOT/tools/c_cpp_properties.json.$1  $MOROS_ROOT/.vscode/c_cpp_properties.json
     cd $MOROS_ROOT/$1
 }
+
+build()
+{
+    BACKUP_PWD=$(pwd)
+    
+    if [ "$1" == "kernel" ]; then
+        cd $MOROS_ROOT/kernel
+        make clean && clear && make run
+    fi
+
+    if [ "$1" == "exp" ]; then
+        cd $MOROS_ROOT/exp
+        make clean && clear && make && bin/experiments
+    fi
+    
+    cd $BACKUP_PWD
+}
