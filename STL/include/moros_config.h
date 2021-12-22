0aa5083866c998e2fa062cd1853a6fccb18da574
Moros1138 <moros1138@gmail.com>
2021-12-22 16:32:49 -0500

    formatting and better debug macros

--- STL/include/moros_config.h
+++ STL/include/moros_config.h
@@ -1,15 +1,40 @@
-#define USE_MOROS_DEBUG
+#ifndef MOROS_CONFIG_H
+#define MOROS_CONFIG_H
 
-#ifdef USE_MOROS_DEBUG
-#define MOROS_DEBUG(...) printf(__VA_ARGS__)
+
+// debug levels
+//  0 = none (default)
+//  1 = error
+//  2 = info
+#ifndef USE_MOROS_DEBUG
+#define USE_MOROS_DEBUG 0
+#endif
+
+#if USE_MOROS_DEBUG > 0
+#   if USE_MOROS_DEBUG >= 1
+#       define MOROS_ERROR(...) printf(__VA_ARGS__)
+#   else
+#       define MOROS_ERROR(...) do { } while(false)
+#   endif
+
+#   if USE_MOROS_DEBUG >= 2
+#       define MOROS_INFO(...) printf(__VA_ARGS__)
+#   else
+#       define MOROS_INFO(...) do { } while(false)
+#   endif 
 #else
-#define MOROS_DEBUG(...) do { } while(false)
+#   define MOROS_ERROR(...) do { } while(false)
+#   define MOROS_INFO(...) do { } while(false)
 #endif
 
 #if defined(__GLIBCXX__) || defined(__GLIBCPP__)
-#define __MOROS_NAMESPACE__ MorOS
+#   define __MOROS_NAMESPACE__ MorOS
+#include <stddef.h>
+
 #else
-#define __MOROS_NAMESPACE__ std
-#define __MOROS_KERNEL__
+#   define __MOROS_NAMESPACE__ std
+#   define __MOROS_KERNEL__
 #endif
 
+
+#endif // MOROS_CONFIG_H
