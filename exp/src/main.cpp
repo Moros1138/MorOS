b4c5d5cba0c95db753a398381a1b994491f55f4f
Moros1138 <moros1138@gmail.com>
2021-12-23 03:00:08 -0500

    iterator testing

--- exp/src/main.cpp
+++ exp/src/main.cpp
@@ -1,40 +1,23 @@
-#include <iostream>
-#include <memory>
-#include <string>
-#include <vector>
 #include <stdio.h>
 
-#include "moros_allocator.h"
+#define __GLIBCXX__
+#define USE_MOROS_DEBUG 2
 #include "moros_vector.h"
-#include "moros_string.h"
 
 int main()
 {
     MorOS::vector<int> test = { 1, 2, 3, 4, 5 };
-    std::vector<int> stdTest = { 1, 2, 3, 4, 5 };
 
     test.push_back(13);
-    stdTest.push_back(13);
-
     test.emplace_back(14);
-    stdTest.emplace_back(14);
-
     test.insert(test.begin()+2, 11);
-    stdTest.insert(stdTest.begin()+2, 11);
-
     test.emplace(test.begin()+2, 12);
-    stdTest.emplace(stdTest.begin()+2, 12);
 
-    for(auto &i : test)
-        std::cout << i << " ";
+    for(auto it = test.begin(); it != test.end(); it++)
+        printf("%08lx %d\n", it, *it);
     
-    std::cout << std::endl;
-
-
-    for(auto &i : stdTest)
-        std::cout << i << " ";
+    printf("\n");
     
-    std::cout << std::endl;
-
+ 
     return 0;
 }
